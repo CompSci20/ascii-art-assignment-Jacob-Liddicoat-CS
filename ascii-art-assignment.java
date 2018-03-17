458358df46cd980004d6ea6ae553d1fdb19151d8
Jacob Liddicoat <j.liddicoat@share.epsb.ca>
2018-03-17 15:20:18 +0000

    Add Letter Strings

--- ascii-art-assignment.java
+++ ascii-art-assignment.java
@@ -2,3 +2,17 @@
 //Date created: Mar 17,2018
 public class ascii-art-assignment
  {
+  public static void main(String args[])
+  {
+   String JJJJJ ="JJJJJ";
+   String J ="J";
+   String A ="A";
+   String AAAAA ="AAAAA";
+   String CCCC ="CCCC";
+   String C ="C";
+   String OOOO ="OOOO";
+   String O ="O";
+   String BBBB ="BBBB";
+   String B ="B";
+
+   
